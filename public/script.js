96b20a7669ef1dd19f859c7ed48c632926a2a2c0
pricken45 <ruben.stenlund@gmail.com>
2023-01-13 14:00:17 +0100

    initial commit

---
+++ public/script.js
@@ -0,0 +1,13 @@
+let socket = io();
+
+let n1 = document.getElementById("alt1");
+let n2 = document.getElementById("alt2");
+let n3 = document.getElementById("alt3");
+let n4 = document.getElementById("alt4");
+
+let c1 = document.getElementById("add");
+let c2 = document.getElementById("sub");
+let c3 = document.getElementById("mult");
+let c4 = document.getElementById("div");
+
+let usrbtn = [c1,c2,c3,c4];
